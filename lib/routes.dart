3691f6f0289899f850e6e0cb70db36ae00f81937
michelleangela07 <88884488+michelleangela07@users.noreply.github.com>
2023-10-18 20:51:12 +0700

    route

---
+++ lib/routes.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/widgets.dart';
+//import 'package:picfresh/screens/categories/dessert/categories_dessert.dart';
+//import 'package:picfresh/screens/categories/drinks/categories_drinks.dart';
+//import 'package:picfresh/screens/categories/meat/categories_meat.dart';
+//import 'package:picfresh/screens/categories/seafood/categories_seafood.dart';
+//import 'package:picfresh/screens/categories/veggies/categories_veggies.dart';
+//import 'package:picfresh/screens/complete_profile/complete_profile_screen.dart';
+//import 'package:picfresh/screens/details/details_screen.dart';
+//import 'package:picfresh/screens/forgot_password/forgot_password_screen.dart';
+//import 'package:picfresh/screens/home/home_screen.dart';
+import 'package:picfresh/screens/login_success/login_success_screen.dart';
+//import 'package:picfresh/screens/otp/otp_screen.dart';
+import 'package:picfresh/screens/sign_in/sign_in_screen.dart';
+import 'package:picfresh/screens/splash/splash_screen.dart';
+import 'package:picfresh/screens/sign_up/sign_up_screen.dart';
+//import 'package:picfresh/screens/article/article_screen.dart';
+//import 'package:picfresh/screens/recipe/recipe_screen.dart';
+//import 'package:picfresh/screens/favorite/favorite_screen.dart';
+//import 'package:picfresh/screens/profile/profile_screen.dart';
+
+final Map<String, WidgetBuilder> routes = {
+  SplashScreen.routeName: (context) => SplashScreen(),
+  SignInScreen.routeName: (context) => SignInScreen(),
+  //ForgotPasswordScreen.routeName: (context) => ForgotPasswordScreen(),
+  LoginSuccessScreen.routeName: (context) => LoginSuccessScreen(),
+  SignUpScreen.routeName: (context) => SignUpScreen(),
+  //CompleteProfileScreen.routeName: (context) => CompleteProfileScreen(),
+  //OtpScreen.routeName: (context) => OtpScreen(),
+  //HomeScreen.routeName: (context) => HomeScreen(),
+  //ArticleScreen.routeName: (context) => ArticleScreen(),
+  //DetailsScreen.routeName: (context) => DetailsScreen(),
+  //RecipeScreen.routeName: (context) => RecipeScreen(),
+  //CategoriesMeat.routeName: (context) => CategoriesMeat(),
+  //CategoriesSeafood.routeName: (context) => CategoriesSeafood(),
+  //CategoriesVeggies.routeName: (context) => CategoriesVeggies(),
+  //CategoriesDessert.routeName: (context) => CategoriesDessert(),
+  //CategoriesDrinks.routeName: (context) => CategoriesDrinks(),
+  //FavoriteScreen.routeName: (context) => FavoriteScreen(),
+  //ProfileScreen.routeName: (context) => ProfileScreen(),
+};
